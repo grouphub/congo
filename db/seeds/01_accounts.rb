99b8ca72af1ea08c3dee2d866edacf1e6172b5c4
Roger Jungemann <roger@thefifthcircuit.com>
2014-10-31 20:23:39 -0700

    add admin seeds

--- db/seeds/01_accounts.rb
+++ db/seeds/01_accounts.rb
@@ -1,4 +1,8 @@
 Account.create! \
+  name: 'Admin Account',
+  tagline: 'GroupHub administrative account'
+
+Account.create! \
   name: 'First Account',
   tagline: 'First account is best account!'
 
