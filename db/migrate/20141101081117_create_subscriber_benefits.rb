adf596bef06d3c0732e83af123a29d65e4222085
allenbyerly <allen@tritl.com>
2014-11-01 09:36:17 -0700

    added subscriber_benefits to db

---
+++ db/migrate/20141101081117_create_subscriber_benefits.rb
@@ -0,0 +1,22 @@
+class CreateSubscriberBenefits < ActiveRecord::Migration
+  def change
+    create_table :subscriber_benefits do |t|
+      t.integer   :carrier_id
+      t.string    :master_policy_number 
+      t.integer   :group_id
+      t.string    :group_or_policy_number
+      t.integer   :member_id
+      t.string 	:enrollment_reference_number
+      t.date 	:enrollment_date
+      t.integer   :subscriber_number
+      t.integer   :benefit_id
+      t.date      :benefit_begin_date
+      t.string    :benefit_type
+      t.boolean   :benefit_late_enrollment
+      t.string    :benefit_maintenance_type: "Addition"
+
+      t.timestamps
+    end
+  end
+end
+
