f9066d7464f6d2119634ce9642bf4775cd742e69
Roger Jungemann <roger@thefifthcircuit.com>
2014-11-11 03:43:16 -0800

    oops, also fix flashes factory for Heroku

--- app/assets/javascripts/factories/flashes-factory.js
+++ app/assets/javascripts/factories/flashes-factory.js
@@ -1,18 +1,20 @@
 var congoApp = angular.module('congoApp');
 
-congoApp.factory('flashesFactory', function (eventsFactory) {
-  var flashes = [];
+congoApp.factory('flashesFactory', [
+  function (eventsFactory) {
+    var flashes = [];
 
-  return {
-    flashes: flashes,
-    add: function (type, message) {
-      flashes.push({
-        type: type,
-        message: message
-      });
+    return {
+      flashes: flashes,
+      add: function (type, message) {
+        flashes.push({
+          type: type,
+          message: message
+        });
 
-      eventsFactory.emit('flashes:added');
-    }
-  };
-});
+        eventsFactory.emit('flashes:added');
+      }
+    };
+  }
+]);
 
