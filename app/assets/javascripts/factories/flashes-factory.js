c232f5b36a15ec3d68395c7032508e055744ad15
Roger Jungemann <roger@thefifthcircuit.com>
2014-11-18 23:25:58 -0800

    fixed flashes

--- app/assets/javascripts/factories/flashes-factory.js
+++ app/assets/javascripts/factories/flashes-factory.js
@@ -4,7 +4,7 @@ congoApp.factory('flashesFactory', [
   'eventsFactory',
   function (eventsFactory) {
     var flashes = [];
-
+    
     return {
       flashes: flashes,
       add: function (type, message) {
