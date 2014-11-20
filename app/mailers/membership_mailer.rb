d47e0a058a876a30f47176ac62e8d7c993833b27
allenbyerly <allen@tritl.com>
2014-11-20 03:13:46 -0800

    updated invitation email with formating

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -4,12 +4,16 @@ class MembershipMailer < ActionMailer::Base
   def confirmation_email(membership, request)
     email = membership.email
     email_token = membership.email_token
+    group_name = membership.group.name
+
+    @email = membership.email
+    @group = membership.group.name
     @url = "#{request.protocol}#{request.host_with_port}/users/new_customer?email_token=#{email_token}"
 
     # TODO: Add account name
     # TODO: Finish laying out email
     # TODO: Finish routing for customer
-    mail(to: email, subject: "You've been invited to join GroupHub!")
+    mail(to: email, subject: "You've been invited to join #{group_name}")
   end
 end
 
