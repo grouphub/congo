6df8fdb754d95c2da35aad59be43a96dc5bfee1c
Roger Jungemann <roger@thefifthcircuit.com>
2014-11-23 17:18:15 -0800

    fix permissions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,24 +11,28 @@ class HomeController < ApplicationController
       unless current_user
         flash[:error] = 'You must be signed in to continue.'
         redirect_to '/users/sign_in'
+        return
       end
 
       current_account = Account.where(slug: account_slug).first
       unless current_account
         flash[:error] = 'We could not find an appropriate account.'
         redirect_to '/'
+        return
       end
 
       is_part_of_account = current_user.roles.any? { |role| role.account_id == current_account.id }
       unless is_part_of_account
         flash[:error] = 'We could not find an appropriate account.'
         redirect_to '/'
+        return
       end
 
       plan_name = current_account.plan_name || ''
-      unless current_user.invitation || Account::PLAN_NAMES.include?(plan_name)
+      if Account::PLAN_NAMES.include?(plan_name) && !current_user.invitation
         flash[:error] = 'Please choose a valid plan before continuing.'
         redirect_to '/users/new_plan'
+        return
       end
     end
 
@@ -36,6 +40,7 @@ class HomeController < ApplicationController
       unless current_user.roles.any? { |role| role.name == 'admin' }
         flash[:error] = 'You must be an admin to continue.'
         redirect_to '/'
+        return
       end
     end
   end
