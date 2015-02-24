ea86ddf75de022b98ee243e41835374f45d34893
Roger Jungemann <roger@thefifthcircuit.com>
2015-02-23 19:41:44 -0500

    populate account edit form

--- app/assets/javascripts/controllers/accounts-edit-controller.js
+++ app/assets/javascripts/controllers/accounts-edit-controller.js
@@ -10,7 +10,15 @@ congoApp.controller('AccountsEditController', [
     $http
       .get('/api/v1/accounts/' + $scope.accountSlug() + '/roles/' + $scope.currentRole() + '/properties/accounts.json')
       .success(function (data, status, headers, config) {
+        var currentAccount = $scope.currentAccount();
+        var currentAccountProperties = JSON.parse(currentAccount.properties_data);
+
         $scope.elements = data.elements;
+        $scope.planName = currentAccount.plan_name;
+
+        _($scope.elements).each(function (element) {
+          element.value = currentAccountProperties[element.name];
+        });
 
         $scope.ready();
       })
