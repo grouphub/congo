a8ca75dd11802a2b8ef7d765847ab8648e24eaa0
Roger Jungemann <roger@thefifthcircuit.com>
2015-03-05 13:58:40 -0800

    make sure propertied records are saved with a default value

--- app/models/concerns/propertied.rb
+++ app/models/concerns/propertied.rb
@@ -1,6 +1,14 @@
 module Propertied
   extend ActiveSupport::Concern
 
+  included do
+    before_save :populate_properties
+  end
+
+  def populate_properties
+    self.properties_data ||= {}.to_json
+  end
+
   def properties=(hash)
     self.properties_data = hash.to_json
   end
