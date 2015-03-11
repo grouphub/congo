d2da9c19356f94651d713a9f34f1df3993e6c1d1
Roger Jungemann <roger@thefifthcircuit.com>
2015-03-11 00:13:12 -0700

    finish testing application

--- spec/features/customers/groups_spec.rb
+++ spec/features/customers/groups_spec.rb
@@ -44,12 +44,182 @@ describe 'As a customer', js: true do
       expect(page).to have_content('Benefits Enrollment Application')
 
       scroll_to_element_in('h3:contains("Primary Applicant")', 'main')
-
       within '[name="subscriber"]' do
         expect(page).to have_content('Primary Applicant')
+
+        fill_in 'First Name', with: 'Candice'
+        fill_in 'Middle Name', with: 'C.'
+        fill_in 'Last Name', with: 'Customer'
+        choose 'Female'
+        fill_in 'Social Security Number', with: '444-44-4444'
+        fill_in 'Date of Birth (mm/dd/yyyy)', with: '04/04/1984'
       end
 
-      # binding.pry
+      scroll_to_element_in('h3:contains("Contact Information")', 'main')
+      within '[name="subscriberContact"]' do
+        expect(page).to have_content('Contact Information')
+
+        fill_in 'Street Address', with: '444 Carrie Ct.'
+        fill_in 'Apt. #', with: '44'
+        fill_in 'City', with: 'Cambridge'
+        fill_in 'State', with: 'CA'
+        fill_in 'ZIP', with: '44444'
+        fill_in 'County', with: 'Contra Costa'
+        fill_in 'Phone', with: '(444) 444-4444'
+        fill_in 'Other Phone', with: '(444) 444-5555'
+      end
+
+      scroll_to_element_in('h3:contains("Previous Coverage")', 'main')
+      within '[name="previousCoverage"]' do
+        expect(page).to have_content('Previous Coverage')
+
+        choose('Yes')
+        fill_in 'Medical Record # (If Any)', with: '4444444'
+        fill_in 'If Yes, Most Recent Insurance Carrier', with: 'Anthem Blue Cross'
+        fill_in 'Dates of Coverage', with: '04/2014-08/2014'
+      end
+
+      scroll_to_element_in('h3:contains("Dependents to be Covered")', 'main')
+      within '[name="dependents"]' do
+        expect(page).to have_content('Dependents to be Covered')
+
+        all('a', text: 'Add Dependent').first.click
+
+        expect(page).to have_content('Dependent 1')
+
+        fill_in 'First Name', with: 'Corwin'
+        fill_in 'Middle Name', with: 'C.'
+        fill_in 'Last Name', with: 'Customer'
+        choose 'Male'
+        fill_in 'Medical Record # (If Any)', with: '4444444'
+        fill_in 'Social Security Number', with: '444-44-4445'
+        fill_in 'Date of Birth (mm/dd/yyyy)', with: '04/04/2004'
+        choose 'Yes'
+        fill_in 'If Yes, Most Recent Insurance Carrier', with: 'Anthem Blue Cross'
+        fill_in 'Dates of Coverage', with: '04/2014-08/2014'
+      end
+
+      scroll_to_element_in('h3:contains("Parent or Legal Guardian")', 'main')
+      within '[name="parentOrGuardian"]' do
+        expect(page).to have_content('Parent or Legal Guardian')
+
+        choose 'Yes'
+
+        fill_in 'First Name', with: 'Cal'
+        fill_in 'Middle Name', with: 'C.'
+        fill_in 'Last Name', with: 'Customer'
+        choose 'Male'
+        fill_in 'Medical Record # (If Any)', with: '4444444'
+        fill_in 'Social Security Number', with: '444-44-4443'
+        fill_in 'Date of Birth (mm/dd/yyyy)', with: '04/04/1982'
+        choose 'Spouse'
+        fill_in 'Preferred Language Spoken (If Not English)', with: 'Spanish'
+        fill_in 'Preferred Language Read (If Not English)', with: 'Spanish'
+
+        within '.same-address' do
+          choose 'No'
+        end
+
+        fill_in 'Street Address', with: '444 Carrie Ct.'
+        fill_in 'Apt. #', with: '44'
+        fill_in 'City', with: 'Cambridge'
+        fill_in 'State', with: 'CA'
+        fill_in 'ZIP', with: '44444'
+        fill_in 'County', with: 'Contra Costa'
+        fill_in 'Phone', with: '(444) 444-4444'
+        fill_in 'Other Phone', with: '(444) 444-5555'
+      end
+
+      scroll_to_element_in('h3:contains("You Can Choose an Authorized Representative")', 'main')
+      within '[name="authorizedRepresentative"]' do
+        expect(page).to have_content('You Can Choose an Authorized Representative')
+
+        choose 'Yes'
+
+        fill_in 'First Name', with: 'Cal'
+        fill_in 'Middle Name', with: 'C.'
+        fill_in 'Last Name', with: 'Customer'
+        fill_in 'Street Address', with: '444 Carrie Ct.'
+        fill_in 'Apt. #', with: '44'
+        fill_in 'City', with: 'Cambridge'
+        fill_in 'State', with: 'CA'
+
+        scroll_by 500, 'main'
+
+        fill_in 'ZIP', with: '44444'
+        fill_in 'Phone', with: '(444) 444-4444'
+
+        fill_in 'Primary Applicant, Parent, or Legal Guardian for Applicants Under Age 18', with: 'Cal C. Customer'
+        fill_in 'Date (mm/dd/yyyy)', with: '03/10/2015'
+      end
+
+      all('input[type=submit]').first.click
+
+      # TODO: Make sure there's a flash message
+
+      expect(page).to have_content('Welcome, Candice!')
+
+      application = Application.last
+      properties = application.properties
+
+      expect(properties['first_name']).to eq('Candice')
+      expect(properties['middle_name']).to eq('C.')
+      expect(properties['last_name']).to eq('Customer')
+      expect(properties['sex_indicate']).to eq('Female')
+      expect(properties['social_security_number']).to eq('444-44-4444')
+      expect(properties['date_of_birth']).to eq('04/04/1984')
+      expect(properties['street_address']).to eq('444 Carrie Ct.')
+      expect(properties['apartment_number']).to eq('44')
+      expect(properties['city']).to eq('Cambridge')
+      expect(properties['state']).to eq('CA')
+      expect(properties['zip']).to eq('44444')
+      expect(properties['county']).to eq('Contra Costa')
+      expect(properties['phone']).to eq('(444) 444-4444')
+      expect(properties['other_phone']).to eq('(444) 444-5555')
+      expect(properties['coverage_experience']).to eq('Yes')
+      expect(properties['medical_record_number']).to eq('4444444')
+      expect(properties['if_yes_most_recent_insurance_carrier']).to eq('Anthem Blue Cross')
+      expect(properties['dates_of_coverage']).to eq('04/2014-08/2014')
+      expect(properties['dependent_1_first_name']).to eq('Corwin')
+      expect(properties['dependent_1_middle_name']).to eq('C.')
+      expect(properties['dependent_1_last_name']).to eq('Customer')
+      expect(properties['dependent_1_sex_indicate']).to eq('Male')
+      expect(properties['dependent_1_medical_record_number_if_any']).to eq('4444444')
+      expect(properties['dependent_1_social_security_number']).to eq('444-44-4445')
+      expect(properties['dependent_1_date_of_birth']).to eq('04/04/2004')
+      expect(properties['dependent_1_coverage_experience']).to eq('Yes')
+      expect(properties['dependent_1_if_yes_most_recent_insurance_carrier']).to eq('Anthem Blue Cross')
+      expect(properties['dependent_1_dates_of_coverage']).to eq('04/2014-08/2014')
+      expect(properties['parent_or_legal_guardian_first_name']).to eq('Cal')
+      expect(properties['parent_or_legal_guardian_middle_name']).to eq('C.')
+      expect(properties['parent_or_legal_guardian_last_name']).to eq('Customer')
+      expect(properties['parent_or_legal_guardian_sex_indicate']).to eq('Male')
+      expect(properties['parent_or_legal_guardian_medical_record_number_if_any']).to eq('4444444')
+      expect(properties['parent_or_legal_guardian_social_security_number']).to eq('444-44-4443')
+      expect(properties['parent_or_legal_guardian_date_of_birth']).to eq('04/04/1982')
+      expect(properties['parent_or_legal_guardian_rel_to_applicant']).to eq('Spouse')
+      expect(properties['parent_or_legal_guardian_language_spoken']).to eq('Spanish')
+      expect(properties['parent_or_legal_guardian_language_read']).to eq('Spanish')
+      expect(properties['parent_or_legal_guardian_same_address']).to eq('No')
+      expect(properties['parent_or_legal_guardian_street_address']).to eq('444 Carrie Ct.')
+      expect(properties['parent_or_legal_guardian_apartment_number']).to eq('44')
+      expect(properties['parent_or_legal_guardian_city']).to eq('Cambridge')
+      expect(properties['parent_or_legal_guardian_state']).to eq('CA')
+      expect(properties['parent_or_legal_guardian_zip']).to eq('44444')
+      expect(properties['parent_or_legal_guardian_county']).to eq('Contra Costa')
+      expect(properties['parent_or_legal_guardian_phone']).to eq('(444) 444-4444')
+      expect(properties['parent_or_legal_guardian_other_phone']).to eq('(444) 444-5555')
+      expect(properties['authorized_representative_first_name']).to eq('Cal')
+      expect(properties['authorized_representative_middle_name']).to eq('C.')
+      expect(properties['authorized_representative_last_name']).to eq('Customer')
+      expect(properties['authorized_representative_street_address']).to eq('444 Carrie Ct.')
+      expect(properties['authorized_representative_apartment_number']).to eq('44')
+      expect(properties['authorized_representative_city']).to eq('Cambridge')
+      expect(properties['authorized_representative_street']).to eq('CA')
+      expect(properties['authorized_representative_zip']).to eq('44444')
+      expect(properties['authorized_representative_phone']).to eq('(444) 444-4444')
+      expect(properties['authorized_representative_primary_applicant']).to eq('Cal C. Customer')
+      expect(properties['authorized_representative_date']).to eq('03/10/2015')
     end
 
   end
