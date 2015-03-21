c50074c947eef9e076097724c7febf4c6865bd11
Roger Jungemann <roger@thefifthcircuit.com>
2015-03-20 23:11:29 -0600

    fix s3 initializer in prod

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -5,9 +5,18 @@ secret_key = Rails.application.config.s3.secret_key
 host = Rails.application.config.s3.host
 port = Rails.application.config.s3.port
 
-AWS::S3::Base.establish_connection! \
+args = {
   access_key_id: access_key,
-  secret_access_key: secret_key,
-  server: host,
-  port: port
+  secret_access_key: secret_key
+}
+
+if host
+  args[:server] = host
+end
+
+if port
+  args[:port] = port
+end
+
+AWS::S3::Base.establish_connection!(args)
 
