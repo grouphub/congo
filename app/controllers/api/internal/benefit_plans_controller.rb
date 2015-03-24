5d14358eb1c8cb0e5624d5873d956f5822e7f4e2
Roger Jungemann <roger@thefifthcircuit.com>
2015-03-23 22:40:28 -0600

    fix bug where carrier account may be nil

--- app/controllers/api/internal/benefit_plans_controller.rb
+++ app/controllers/api/internal/benefit_plans_controller.rb
@@ -143,12 +143,16 @@ class Api::Internal::BenefitPlansController < ApplicationController
   # Render methods
 
   def render_benefit_plan(benefit_plan)
+    # NOTE: Carrier account may be nil if it was deleted by the user.
     carrier_account = benefit_plan.carrier_account
+    account = carrier_account.try(:account)
+    carrier = carrier_account.try(:carrier)
+    carrier_account_hash = carrier_account.try(:as_json) || {}
 
     benefit_plan.as_json.merge({
-      'carrier_account' => carrier_account.as_json.merge({
-        'account' => carrier_account.account,
-        'carrier' => carrier_account.carrier
+      'carrier_account' => carrier_account_hash.merge({
+        'account' => account,
+        'carrier' => carrier
       }),
       'attachments' => benefit_plan.attachments
     })
