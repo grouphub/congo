58340fe5ba2e299b99e6de275cf6b37c5b4fcc29
Roger Jungemann <roger@thefifthcircuit.com>
2015-04-13 16:33:39 -0700

    chart fix

--- app/controllers/api/internal/charts_controller.rb
+++ app/controllers/api/internal/charts_controller.rb
@@ -6,44 +6,44 @@ class Api::Internal::ChartsController < ApplicationController
   before_filter :ensure_broker_or_group_admin!
 
   def members_status
-    start_data = {
-      submitted: 0,
-      completed: 0,
-      selected: 0,
-      not_started: 0,
+    keys = {
+      submitted: 4,
+      completed: 3,
+      selected: 2,
+      not_started: 1,
       invited: 0
     }
 
+    start_data = [0, 0, 0, 0, 0]
     series = ['Members']
     labels = ['Invited', 'Not Started', 'Selected', 'Completed', 'Submitted']
-    data = [0, 0, 0, 0, 0]
 
-    hash = current_account.memberships
+    data = current_account.memberships
       .includes(:applications)
-      .inject(start_data) { |hash, membership|
+      .inject(start_data) { |list, membership|
         application = membership.applications.last
 
-        next(hash) if application.nil?
-
-        if application.submitted_by_id
+        if !application
+          # User has signed up but hasn't enrolled yet.
+          list[keys[:invited]] += 1
+        elsif application.submitted_by_id
           # Broker sent out application.
-          hash[4] += 1
+          list[keys[:submitted]] += 1
         elsif application.completed_by_id
           # User completed application.
-          hash[3] += 1
+          list[keys[:completed]] += 1
         elsif application.selected_by_id
           # User selected an application.
-          hash[2] += 1
+          list[keys[:selected]] += 1
         elsif membership.user_id.nil?
           # User hasn't signed up yet.
-          hash[0] += 1
-        else
-          # User has signed up but hasn't enrolled yet.
-          hash[1] += 1
+          list[keys[:not_started]] += 1
         end
 
-        hash
+        list
       }
+
+    pp data
 
     respond_to do |format|
       format.json {
