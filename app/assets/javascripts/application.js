2ff3f1f2f8253413e603f62ef439f2b75401971c
Marco Gallardo <marco.gallardo@tangosource.com>
2015-09-03 15:35:49 -0500

    adding basic configuration for new Group's Add Members page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -74,6 +74,7 @@
 //= require controllers/groups-show-controller
 //= require controllers/groups-welcome-controller
 //= require controllers/groups-details-controller
+//= require controllers/groups-members-controller
 //= require controllers/tokens-index-controller
 //= require controllers/users-new-controller
 //= require controllers/users-new-account-controller
