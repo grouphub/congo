8396ffd2205ed834abf599a3a4be5ce46f36c898
Marco Gallardo <marco.gallardo@tangosource.com>
2015-09-07 12:56:46 -0500

    adding phone field to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903181600) do
+ActiveRecord::Schema.define(version: 20150907170842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -292,6 +292,7 @@ ActiveRecord::Schema.define(version: 20150903181600) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
+    t.string   "phone"
   end
 
   add_index "users", ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
