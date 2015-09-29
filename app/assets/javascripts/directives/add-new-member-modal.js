a201796835a34d7b713f254328caa771ca627750
Marco Gallardo <marco.gallardo@tangosource.com>
2015-09-29 08:50:39 -0500

    Applying angular best practices on groups' directives

--- app/assets/javascripts/directives/add-new-member-modal.js
+++ app/assets/javascripts/directives/add-new-member-modal.js
@@ -1,46 +1,50 @@
-var congoApp = angular.module('congoApp');
+!function() {
+  "use strict";
 
-congoApp.directive('addNewMemberModal', [
-  '$http',
-  '$location',
-  'flashesFactory',
-  function ($http, $location, flashesFactory) {
-    return {
+  angular.module('congoApp').directive('addNewMemberModal', addNewMemberModal);
+
+  addNewMemberModal.$inject = ['$http', '$location', 'flashesFactory'];
+
+  function addNewMemberModal($http, $location, flashesFactory) {
+    var addNewMemberModalDirective = {
       restrict: 'E',
       replace: true,
       templateUrl: congo.assets['directives/add-new-member-modal.html'],
-      link: function ($scope, $element, $attrs) {
-
-        $scope.newMember = {
-          role_name: 'customer',
-          first_name: null,
-          last_name: null,
-          phone: null,
-          email: null
-        };
-
-        $scope.submitNewMember = function() {
-          $http
-            .post('/api/internal/accounts/' + $scope.accountSlug() + '/roles/' + $scope.currentRole() + '/groups/' + $scope.groupSlug() + '/memberships.json', $scope.newMember)
-            .then(
-              function (response) {
-                congo.currentUser = response.data.user;
-
-                flashesFactory.add('success', 'Successfully added the member.');
-
-                $('#add-new-member-modal').modal('hide');
-              },
-              function (response) {
-                var error = (response.data && response.data.error) ?
-                  response.data.error :
-                  'An error occurred. Please try adding a user again later.';
-
-                flashesFactory.add('danger', error);
-              }
-            );
-        };
-      }
+      link: modal
     };
-  }
-]);
 
+    return addNewMemberModalDirective;
+
+    function modal($scope, $element, $attrs) {
+
+      $scope.newMember = {
+        role_name: 'customer',
+        first_name: null,
+        last_name: null,
+        phone: null,
+        email: null
+      };
+
+      $scope.submitNewMember = function() {
+        $http
+          .post('/api/internal/accounts/' + $scope.accountSlug() + '/roles/' + $scope.currentRole() + '/groups/' + $scope.groupSlug() + '/memberships.json', $scope.newMember)
+          .then(
+            function (response) {
+              congo.currentUser = response.data.user;
+
+              flashesFactory.add('success', 'Successfully added the member.');
+
+              $('#add-new-member-modal').modal('hide');
+            },
+            function (response) {
+              var error = (response.data && response.data.error) ?
+                response.data.error :
+                'An error occurred. Please try adding a user again later.';
+
+              flashesFactory.add('danger', error);
+            }
+          );
+      };
+    }
+  }
+}();
