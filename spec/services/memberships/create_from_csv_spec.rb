e61f9543d118a4019616c1c924cacf5e6700c33b
Antonio Chavez <cavjzz@gmail.com>
2015-09-29 11:30:18 -0500

    Refactored CSV membership creation service from a Functional module to a state-less class

--- spec/services/memberships/create_from_csv_spec.rb
+++ spec/services/memberships/create_from_csv_spec.rb
@@ -1,17 +1,17 @@
-require_relative '../../../app/services/memberships/create_from_csv'
+require_relative "#{Rails.root}/app/services/memberships/create_from_csv"
 
 describe Memberships::CreateFromCSV do
   describe '#call' do
-    let(:group)    { double :group }
+    let(:group)    { double :group, id: 1, account_id: 1 }
     let(:csv_file) { "#{Rails.root}/spec/data/GroupHub_Employee_Template.csv" }
 
-    it 'creates memberships out of csv file' do
-      allow(group).to receive(:id).and_return(1)
-      allow(group).to receive(:account_id).and_return(1)
+    subject { Memberships::CreateFromCSV.(csv_file, group) }
 
-      expect {
-        Memberships::CreateFromCSV.call(csv_file, group)
-      }.to change(Membership, :count).by(2)
+    describe "#process" do
+      it 'creates memberships out of csv file' do
+        expect { subject }.
+          to change(Membership, :count).by(2)
+      end
     end
   end
 end
