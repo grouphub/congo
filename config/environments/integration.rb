011f5d790b4ed4c0f359d9884bd9e5e7bc7521cd
Sven Pedersen <sven.pedersen@gmail.com>
2015-12-08 10:57:07 -0500

    use local logging for integration

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -50,12 +50,12 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  if ENV['PAPERTRAIL_HOST'] && ENV['PAPERTRAIL_PORT']
-    config.logger = RemoteSyslogLogger.new \
-      ENV['PAPERTRAIL_HOST'],
-      ENV['PAPERTRAIL_PORT'],
-      program: ENV['PAPERTRAIL_PROGRAM']
-  end
+  #if ENV['PAPERTRAIL_HOST'] && ENV['PAPERTRAIL_PORT']
+  #  config.logger = RemoteSyslogLogger.new \
+  #    ENV['PAPERTRAIL_HOST'],
+  #    ENV['PAPERTRAIL_PORT'],
+  #    program: ENV['PAPERTRAIL_PROGRAM']
+  #end
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
