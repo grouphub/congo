7c305748b94dde825765b5bf3958d89c043cdaed
Sven Pedersen <sven.pedersen@gmail.com>
2015-12-22 16:30:20 -0500

    move payment controllers to API dir

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,3 +2,9 @@ require 'stripe'
 
 Stripe.api_key = Congo2::Application.config.stripe.publishable_key
 
+#Rails.configuration.stripe = {
+#  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+#  :secret_key      => ENV['STRIPE_SECRET_KEY']
+#}
+
+#Stripe.api_key = Rails.configuration.stripe[:secret_key]
