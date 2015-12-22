6c1cd5bb1c9a2022c0f2dbb941771539059ab53a
Sven Pedersen <sven.pedersen@gmail.com>
2015-12-22 16:30:58 -0500

    add different Stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,12 @@
 require 'stripe'
 
-Stripe.api_key = Congo2::Application.config.stripe.publishable_key
+#Stripe.api_key = Congo2::Application.config.stripe.publishable_key
 
-#Rails.configuration.stripe = {
+Rails.configuration.stripe = {
 #  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :publishable_key => Congo2::Application.config.stripe.publishable_key,
 #  :secret_key      => ENV['STRIPE_SECRET_KEY']
-#}
+  :secret_key => Congo2::Application.config.stripe.secret_key
+}
 
-#Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
