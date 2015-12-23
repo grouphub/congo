2baccf0659cb369cd5fd540dff70f9564d5a4ae2
Sven Pedersen <sven.pedersen@gmail.com>
2015-12-23 10:43:04 -0500

    default to env stripe config

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,10 +3,8 @@ require 'stripe'
 #Stripe.api_key = Congo2::Application.config.stripe.publishable_key
 
 Rails.configuration.stripe = {
-#  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :publishable_key => Congo2::Application.config.stripe.publishable_key,
-#  :secret_key      => ENV['STRIPE_SECRET_KEY']
-  :secret_key => Congo2::Application.config.stripe.secret_key
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] || Congo2::Application.config.stripe.publishable_key,
+  :secret_key => ENV['STRIPE_SECRET_KEY'] || Congo2::Application.config.stripe.secret_key
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
