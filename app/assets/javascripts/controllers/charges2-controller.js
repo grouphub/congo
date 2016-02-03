6004bcb726b25ba0b7edfbbbfe261e4aaafa4575
Allen Byerly <allenbyerly@gmail.com>
2016-02-02 22:43:19 -0800

    built first working payment modal

--- app/assets/javascripts/controllers/charges2-controller.js
+++ app/assets/javascripts/controllers/charges2-controller.js
@@ -2,11 +2,11 @@
   "use strict";
 
   angular.module('congoApp').
-    controller('ChargesModalController', ChargesModalController);
+    controller('Charges2ModalController', ChargesModalController);
 
   ChargesModalController.$inject = [ '$scope', '$http', 'flashesFactory' ];
 
-  function ChargesModalController($scope, $http, flashesFactory) {
+  function Charges2ModalController($scope, $http, flashesFactory) {
     $scope.$on('modal.charges', function(scope, membership, benefitPlans) {
       $scope.membership = membership;
       $scope.benefitPlans = benefitPlans;
